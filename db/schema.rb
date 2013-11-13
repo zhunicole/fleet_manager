34ed51b9e2ea6c5fdf2ad8459dbc3ba801601f84
Nicole Zhu <zhunickyzhu@gmail.com>
2013-11-13 15:02:57 +0800

    user login register reset js now works, with correct format forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113032347) do
+ActiveRecord::Schema.define(version: 20131113044355) do
 
   create_table "activities", force: true do |t|
     t.datetime "created_at"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20131113032347) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
